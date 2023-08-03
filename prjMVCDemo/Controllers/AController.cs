86f14bcb6506ee900c666f59b2abcf2b4477b13b
Cyrene <cyrenetsai.p@gmail.com>
2023-08-03 16:33:02 +0800

    加入專案檔案。

---
+++ prjMVCDemo/Controllers/AController.cs
@@ -0,0 +1,30 @@
+﻿using prjMVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjMVCDemo.Controllers
+{
+    public class AController : Controller
+    {
+        public string sayHello()    
+        {
+            return "Hello~~~";
+        }
+
+        public string lotto()
+        {
+            return (new CLottoGen()).getNembers();
+            
+        }
+
+
+        // GET: A  
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjMVCDemo/Models/CLottoGen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjMVCDemo.Models
+{
+    public class CLottoGen
+    {
+        public string getNembers()
+        {
+            int count = 0;
+            int[] numbers = new int[6];
+            Random rand = new Random();
+            while(count < 6)
+            {
+                int temp = rand.Next(1, 50);
+                numbers[count] = temp;
+                count++;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = 0; j< numbers.Length; j++) 
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    { 
+                        int big = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = big;
+                    }
+
+                    string s = " ";
+                    foreach (int i in numbers)
+                        s += i + " ";
+                    return s;
+        }
+    }
+}
